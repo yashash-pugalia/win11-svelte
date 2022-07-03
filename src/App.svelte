3d2884c57757357cf14d6a23b86b6ee3f4802ece
Yashash Pugalia <yashash1511@gmail.com>
2022-07-03 14:20:52 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,7 @@
   import "fluent-svelte/theme.css";
   import Desktop from "./components/Desktop.svelte";
   import Taskbar from "./components/Taskbar.svelte";
-  import "global.css";
+  import "./global.css";
 </script>
 
 <Desktop />
