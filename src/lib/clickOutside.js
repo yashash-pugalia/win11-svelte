47ba4e4f8ef32f3e1fc294fff6b54a662df8b81c
Bellisario <72039923+Bellisario@users.noreply.github.com>
2023-06-23 21:25:44 +0200

    fmt: fix code formatting

--- src/lib/clickOutside.js
+++ src/lib/clickOutside.js
@@ -1,18 +1,18 @@
 export default function clickOutside(element, callbackFunction) {
-    function onClick(event) {
-        if (!element.contains(event.target)) {
-            callbackFunction();
-        }
+  function onClick(event) {
+    if (!element.contains(event.target)) {
+      callbackFunction();
     }
+  }
 
-    document.body.addEventListener('click', onClick);
+  document.body.addEventListener("click", onClick);
 
-    return {
-        update(newCallbackFunction) {
-            callbackFunction = newCallbackFunction;
-        },
-        destroy() {
-            document.body.removeEventListener('click', onClick);
-        }
-    }
+  return {
+    update(newCallbackFunction) {
+      callbackFunction = newCallbackFunction;
+    },
+    destroy() {
+      document.body.removeEventListener("click", onClick);
+    },
+  };
 }
