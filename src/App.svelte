ee215460bf4df9491987feb163f7d255c6a5191a
Yashash Pugalia <yashash1511@gmail.com>
2024-05-08 17:47:01 +0530

    move the bootscreen to app.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,32 @@
 <script lang="ts">
+  import { ProgressRing } from "fluent-svelte";
+  import { onMount } from "svelte";
   import Desktop from "./Desktop.svelte";
   import Taskbar from "./Taskbar.svelte";
+
+  onMount(() => {
+    window.onload = () => {
+      setTimeout(() => {
+        document.querySelector<HTMLElement>(".bootscreen").style.pointerEvents =
+          "none";
+        document.querySelector<HTMLElement>(".bootscreen").style.opacity = "0";
+      }, 100);
+    };
+  });
 </script>
 
 <Desktop />
 <Taskbar />
+
+<div
+  class="bootscreen absolute inset-0 flex flex-col items-center justify-center gap-[10%] bg-black"
+  style="transition: opacity 750ms cubic-bezier(0.6, 0, 0.4, 1)"
+>
+  <img
+    src="/img/icon/Start.png"
+    alt="Loading..."
+    class="w-40 rounded-lg"
+    style="animation: fadein 1500ms linear, scalein 1000ms cubic-bezier(0, 0, 0, 1);"
+  />
+  <ProgressRing />
+</div>
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,6 @@
 import "fluent-svelte/theme.css";
-import "./global.css";
-import { ProgressRing } from "fluent-svelte";
 import App from "./App.svelte";
-
-window.onload = () => {
-  setTimeout(() => {
-    document.querySelector<HTMLElement>(".bootscreen").style.pointerEvents =
-      "none";
-    document.querySelector<HTMLElement>(".bootscreen").style.opacity = "0";
-  }, 100);
-};
-
-const loader = new ProgressRing({
-  target: document.querySelector(".bsload"),
-});
+import "./global.css";
 
 const app = new App({
   target: document.getElementById("app"),
