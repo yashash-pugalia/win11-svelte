8ab327c762aba99ca457dcb829cab6ba660e2aac
Aworldc <83331712+Aworldc@users.noreply.github.com>
2024-05-08 16:36:01 +1000

    Revert "Added the win11 boot animation as a loading screen"
    This reverts commit 40ba8cf71182946d36f29622dfc27fd58d0d6caa.

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,10 @@
-import 'fluent-svelte/theme.css'
-import './global.css'
-import { ProgressRing } from 'fluent-svelte'
-import App from './App.svelte'
+import "fluent-svelte/theme.css";
+import "./global.css";
 
-window.onload = () => {
-    setTimeout(() => {
-        document.querySelector<HTMLElement>('.bootscreen').style.pointerEvents =
-            'none'
-        document.querySelector<HTMLElement>('.bootscreen').style.opacity = '0'
-    }, 100)
-}
-
-const loader = new ProgressRing({
-    target: document.querySelector('.bsload')
-})
+import App from "./App.svelte";
 
 const app = new App({
-    target: document.getElementById('app')
-})
+  target: document.getElementById("app"),
+});
 
-export default app
+export default app;
