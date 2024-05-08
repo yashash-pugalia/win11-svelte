40ba8cf71182946d36f29622dfc27fd58d0d6caa
Aworldc <83331712+Aworldc@users.noreply.github.com>
2024-05-08 16:19:59 +1000

    Added the win11 boot animation as a loading screen

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
-import "fluent-svelte/theme.css";
-import "./global.css";
+import 'fluent-svelte/theme.css'
+import './global.css'
+import { ProgressRing } from 'fluent-svelte'
+import App from './App.svelte'
 
-import App from "./App.svelte";
+window.onload = () => {
+    setTimeout(() => {
+        document.querySelector<HTMLElement>('.bootscreen').style.pointerEvents =
+            'none'
+        document.querySelector<HTMLElement>('.bootscreen').style.opacity = '0'
+    }, 100)
+}
+
+const loader = new ProgressRing({
+    target: document.querySelector('.bsload')
+})
 
 const app = new App({
-  target: document.getElementById("app"),
-});
+    target: document.getElementById('app')
+})
 
-export default app;
+export default app
